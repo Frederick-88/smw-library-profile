66374e4b95ecff7f33c955ef8265ba1cc211710b
Chen Frederick <frederickfd88@gmail.com>
2022-04-18 22:32:46 +0700

    Firebase Import on Vue App

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { firestorePlugin } from "vuefire";
 
-Vue.config.productionTip = false
+Vue.use(firestorePlugin); // defined so we can use 'firestore: {}' in a vue component.
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
