4dde39bec75c65bd4890ebadce61d9135d2cc75e
Chen Frederick <frederickfd88@gmail.com>
2022-04-23 23:06:21 +0700

    Updated Backend Firebase Necessities

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -14,63 +14,153 @@ const firebaseConfig = {
 const database = firebase.initializeApp(firebaseConfig).firestore();
 const storageRef = firebase.storage().ref();
 
-// -----------------
-// Todo List Section
-// -----------------
-// const todolistCollection = database.collection("todolist");
+// ------------------
+// Facilities Section
+// ------------------
+const facilitiesCollection = database.collection("library-facilities");
 
-// const createTodoList = (todoTask) => {
-//   todolistCollection.add({
-//     task: todoTask,
-//   });
-// };
+const fbCreateFacility = (data) => {
+  facilitiesCollection.add({
+    title: data.title,
+    description: data.description,
+    image_thumbnail: data.image_thumbnail,
+    images: data.images,
+  });
+};
 
-// const updateTodoList = (todoId, todoTask) => {
-//   todolistCollection.doc(todoId).update({
-//     task: todoTask,
-//   });
-// };
+const fbUpdateFacility = (facilityId, data) => {
+  facilitiesCollection.doc(facilityId).update({
+    title: data.title,
+    description: data.description,
+    image_thumbnail: data.image_thumbnail,
+    images: data.images,
+  });
+};
 
-// const deleteTodoList = (todoId) => {
-//   todolistCollection.doc(todoId).delete();
-// };
+const fbDeleteFacility = (facilityId) => {
+  facilitiesCollection.doc(facilityId).delete();
+};
 
 // ------------------------
-// Customer Records Section
+// Library Programs Section
 // ------------------------
-// const customerRecordsCollection = database.collection("customer-records");
+const libraryProgramsCollection = database.collection("library-programs");
 
-// const createCustomerRecord = (recordObj) => {
-//   customerRecordsCollection.add({
-//     name: recordObj.name,
-//     email: recordObj.email,
-//     phone_number: recordObj.phone_number,
-//     image_url: recordObj.image_url
-//   });
-// };
+const fbCreateLibraryProgram = (data) => {
+  libraryProgramsCollection.add({
+    title: data.title,
+    description: data.description,
+    date: data.date,
+    image_thumbnail: data.image_thumbnail,
+    images: data.images,
+  });
+};
 
-// const updateCustomerRecord = (customerId, recordObj) => {
-//   customerRecordsCollection.doc(customerId).update({
-//     name: recordObj.name,
-//     email: recordObj.email,
-//     phone_number: recordObj.phone_number,
-//     image_url: recordObj.image_url
-//   });
-// };
+const fbUpdateLibraryProgram = (programId, data) => {
+  libraryProgramsCollection.doc(programId).update({
+    title: data.title,
+    description: data.description,
+    date: data.date,
+    image_thumbnail: data.image_thumbnail,
+    images: data.images,
+  });
+};
 
-// const deleteCustomerRecord = (customerId) => {
-//   customerRecordsCollection.doc(customerId).delete();
-// };
-// -----------------
-// -----------------
+const fbDeleteLibraryProgram = (programId) => {
+  libraryProgramsCollection.doc(programId).delete();
+};
+
+// ------------------------
+// Book Collections Section
+// ------------------------
+const bookCollection = database.collection("library-book-collections");
+
+const fbCreateBookCollection = (data) => {
+  bookCollection.add({
+    image_thumbnail: data.image_thumbnail,
+    title: data.title,
+  });
+};
+
+const fbUpdateBookCollection = (bookCollectionId, data) => {
+  bookCollection.doc(bookCollectionId).update({
+    image_thumbnail: data.image_thumbnail,
+    title: data.title,
+  });
+};
+
+const fbDeleteBookCollection = (bookCollectionId) => {
+  bookCollection.doc(bookCollectionId).delete();
+};
+
+// ----------------
+// Partners Section
+// ----------------
+const partnersCollection = database.collection("library-partners");
+
+const fbCreatePartner = (data) => {
+  partnersCollection.add({
+    logo_url: data.logo_url,
+  });
+};
+
+const fbUpdatePartner = (partnerId, data) => {
+  partnersCollection.doc(partnerId).update({
+    logo_url: data.logo_url,
+  });
+};
+
+const fbDeletePartner = (partnerId) => {
+  partnersCollection.doc(partnerId).delete();
+};
+
+// -----------------------
+// Library Details Section
+// -----------------------
+const libraryDetailsCollection = database.collection("library-details");
+
+const fbCreateLibraryDetail = (data) => {
+  libraryDetailsCollection.add({
+    organization_structure_image_url: data.organization_structure_image_url,
+    contact_details: {
+      whatsapp_number: data.whatsapp_number,
+      email: data.email,
+      phone_number: data.phone_number,
+    },
+  });
+};
+
+const fbUpdateLibraryDetail = (libraryDetailId, data) => {
+  libraryDetailsCollection.doc(libraryDetailId).update({
+    organization_structure_image_url: data.organization_structure_image_url,
+    contact_details: {
+      whatsapp_number: data.whatsapp_number,
+      email: data.email,
+      phone_number: data.phone_number,
+    },
+  });
+};
+
+const fbDeleteLibraryDetail = (libraryDetailId) => {
+  libraryDetailsCollection.doc(libraryDetailId).delete();
+};
 
 export {
   database,
   storageRef,
-  //   createTodoList,
-  //   updateTodoList,
-  //   deleteTodoList,
-  //   createCustomerRecord,
-  //   updateCustomerRecord,
-  //   deleteCustomerRecord,
+  fbCreateFacility,
+  fbUpdateFacility,
+  fbDeleteFacility,
+  fbCreateLibraryProgram,
+  fbUpdateLibraryProgram,
+  fbDeleteLibraryProgram,
+  fbCreateBookCollection,
+  fbUpdateBookCollection,
+  fbDeleteBookCollection,
+  fbCreatePartner,
+  fbUpdatePartner,
+  fbDeletePartner,
+  fbCreateLibraryDetail,
+  fbUpdateLibraryDetail,
+  fbDeleteLibraryDetail,
 };
