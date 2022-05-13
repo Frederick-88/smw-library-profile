15d6a89da041a5701cc834436902c088c66a9f9f
Chen Frederick <frederickfd88@gmail.com>
2022-05-14 00:03:27 +0700

    Setup Vue Router List

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,68 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/homepage/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  }
-]
+    path: "/admin/login",
+    name: "AdminLogin",
+    component: () =>
+      import(
+        /* webpackChunkName: "admin-login" */ "../views/admin/AdminLogin.vue"
+      ),
+  },
+  {
+    path: "/admin/facilities",
+    name: "AdminFacilities",
+    component: () =>
+      import(
+        /* webpackChunkName: "admin-facilities" */ "../views/admin/AdminFacilities.vue"
+      ),
+  },
+  {
+    path: "/admin/programs",
+    name: "AdminPrograms",
+    component: () =>
+      import(
+        /* webpackChunkName: "admin-programs" */ "../views/admin/AdminPrograms.vue"
+      ),
+  },
+  {
+    path: "/admin/book-collections",
+    name: "AdminBookCollections",
+    component: () =>
+      import(
+        /* webpackChunkName: "admin-book-collections" */ "../views/admin/AdminBookCollections.vue"
+      ),
+  },
+  {
+    path: "/admin/partners",
+    name: "AdminPartners",
+    component: () =>
+      import(
+        /* webpackChunkName: "admin-partners" */ "../views/admin/AdminPartners.vue"
+      ),
+  },
+  {
+    path: "/admin/library-details",
+    name: "AdminLibraryDetails",
+    component: () =>
+      import(
+        /* webpackChunkName: "admin-library-details" */ "../views/admin/AdminLibraryDetails.vue"
+      ),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
