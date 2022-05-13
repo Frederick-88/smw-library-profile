3170bb100a007f5dd05c5ee1b6bd0e5ba05daf21
Chen Frederick <frederickfd88@gmail.com>
2022-05-14 00:05:25 +0700

    Added Global Mixins

--- src/mixins/global-mixins.js
+++ src/mixins/global-mixins.js
@@ -1,7 +1,21 @@
+import { mapState } from "vuex";
+
 const globalMixins = {
+  //  add on resize to update adminTableHeight
   computed: {
+    ...mapState(["adminToken"]),
     isMobile() {
       return this.$vuetify.breakpoint.xsOnly;
+    },
+    isOnAdminSite() {
+      return (
+        this.adminToken &&
+        this.$route.path &&
+        this.$route.path.includes("admin")
+      );
+    },
+    adminTableHeight() {
+      return window.innerHeight - 250 - 25 + "px"; // 250px is estimation of navbar, browse layout header and margins. another 25 for more space from bottom.
     },
   },
 };
