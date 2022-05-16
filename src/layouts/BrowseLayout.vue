8426f4b5fce410de89f7f0f369f1932136f647a3
Chen Frederick <frederickfd88@gmail.com>
2022-05-16 14:05:44 +0700

    Componentize Loader

--- src/layouts/BrowseLayout.vue
+++ src/layouts/BrowseLayout.vue
@@ -20,15 +20,7 @@
     </div>
 
     <template v-if="isLoading">
-      <div class="layout-loader__container">
-        <div class="layout-loader">
-          <div class="loader" />
-          <v-icon dense class="loader__middle-icon ml-1">{{
-            headerIcon
-          }}</v-icon>
-        </div>
-        <p class="loader__text">Loading ...</p>
-      </div>
+      <Loader :icon="headerIcon" />
     </template>
     <template v-else>
       <div class="browse-layout__content elevation-10 mt-10 rounded-xl">
@@ -39,8 +31,13 @@
 </template>
 
 <script>
+import Loader from "@/components/utilities/Loader.vue";
+
 export default {
   name: "BrowseLayout",
+  components: {
+    Loader,
+  },
   props: {
     headerTitle: { type: String, default: "Title" },
     headerIcon: { type: String, default: "mdi-eye" },
@@ -91,6 +88,7 @@ export default {
 
     .image-thumbnail {
       width: 125px;
+      min-height: 100px;
       object-fit: contain;
 
       &.image--large {
@@ -109,67 +107,6 @@ export default {
 
     .button-list {
       min-width: 250px;
-    }
-  }
-
-  .layout-loader__container {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 100px;
-
-    .layout-loader {
-      position: relative;
-    }
-
-    .loader__middle-icon {
-      position: absolute;
-      top: 50%;
-      right: 50%;
-      transform: translate(50%, -50%);
-      color: #f9a825;
-      font-size: 2.5rem;
-
-      &.icon-agent {
-        font-size: 2.75rem;
-      }
-    }
-
-    .loader {
-      border: 5px solid black;
-      border-radius: 50%;
-      border-top: 5px solid #f9a825;
-      width: 120px;
-      height: 120px;
-      -webkit-animation: spin 2s linear infinite; /* Safari */
-      animation: spin 2s linear infinite;
-    }
-
-    .loader__text {
-      font-size: 1.125rem;
-      font-weight: 500;
-      margin: 30px 0 0;
-      color: black;
-    }
-
-    /* Safari */
-    @-webkit-keyframes spin {
-      0% {
-        -webkit-transform: rotate(0deg);
-      }
-      100% {
-        -webkit-transform: rotate(360deg);
-      }
-    }
-
-    @keyframes spin {
-      0% {
-        transform: rotate(0deg);
-      }
-      100% {
-        transform: rotate(360deg);
-      }
     }
   }
 }
