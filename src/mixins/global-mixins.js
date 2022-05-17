c71878d2ca13f890984e3028f73c77ffdc33f383
Chen Frederick <frederickfd88@gmail.com>
2022-05-17 23:25:09 +0700

    Updated Global Mixins, for responsiveness purposes

--- src/mixins/global-mixins.js
+++ src/mixins/global-mixins.js
@@ -1,21 +1,50 @@
 import { mapState } from "vuex";
+import { debounce } from "debounce";
 
 const globalMixins = {
-  //  add on resize to update adminTableHeight
+  data() {
+    return {
+      screenHeight: 0,
+    };
+  },
   computed: {
     ...mapState(["adminToken"]),
     isMobile() {
       return this.$vuetify.breakpoint.xsOnly;
     },
+    isTablet() {
+      return this.$vuetify.breakpoint.smOnly;
+    },
+    isNotDesktop() {
+      return this.isMobile || this.isTablet;
+    },
+
+    isOnAdminUrl() {
+      return this.$route.path && this.$route.path.includes("admin");
+    },
     isOnAdminSite() {
-      return (
-        this.adminToken &&
-        this.$route.path &&
-        this.$route.path.includes("admin")
-      );
+      return this.adminToken && this.isOnAdminUrl;
     },
     adminTableHeight() {
-      return window.innerHeight - 250 - 25 + "px"; // 250px is estimation of navbar, browse layout header and margins. another 25 for more space from bottom.
+      return this.screenHeight - 250 - 25 + "px"; // 250px is estimation of navbar, browse layout header and margins. another 25 for more space from bottom.
+    },
+  },
+  created() {
+    window.addEventListener("resize", this.debounceHandleResize);
+  },
+  mounted() {
+    this.handleResize();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.debounceHandleResize);
+  },
+  methods: {
+    debounceHandleResize: debounce(function () {
+      this.handleResize();
+    }, 100),
+
+    handleResize() {
+      this.screenHeight = window.innerHeight;
     },
   },
 };
