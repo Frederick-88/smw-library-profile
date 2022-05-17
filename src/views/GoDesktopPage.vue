a40d0f8dca2d1461f40e2487ad96e89337f3efcb
Chen Frederick <frederickfd88@gmail.com>
2022-05-17 23:24:28 +0700

    Added Go Desktop Page for Admin Page that is accessed from Mobile

---
+++ src/views/GoDesktopPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="go-desktop-page">
+    <h2 class="page-title text-center">
+      Please visit admin page on desktop or bigger screen.
+    </h2>
+    <v-btn href="/" class="mt-7 yellow accent-2">Back to Home</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "GoDesktopPage",
+};
+</script>
+
+<style lang="scss">
+.go-desktop-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .page-title {
+    font-weight: 500;
+    letter-spacing: 1.5px;
+  }
+}
+</style>
