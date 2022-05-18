0a0cf2a135f658675eaf919e93df489f295d0ee1
Chen Frederick <frederickfd88@gmail.com>
2022-05-18 21:25:26 +0700

    Responsiveness Updates App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <v-app>
-    <Navbar />
+    <template v-if="!isShowGoToDesktopPage">
+      <Navbar />
+      <v-main class="pa-0">
+        <router-view />
+      </v-main>
+      <Footer />
+    </template>
 
-    <v-main class="pa-0">
-      <router-view />
-    </v-main>
-
-    <Footer />
+    <template v-else>
+      <go-desktop-page />
+    </template>
   </v-app>
 </template>
 
 <script>
 import Navbar from "@/components/base/Navbar.vue";
 import Footer from "@/components/base/Footer.vue";
+import GoDesktopPage from "./views/GoDesktopPage.vue";
+
+import globalMixins from "@/mixins/global-mixins.js";
 
 // vue-slick-carousel imports
 import "vue-slick-carousel/dist/vue-slick-carousel.css";
@@ -20,9 +27,16 @@ import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 
 export default {
   name: "App",
+  mixins: [globalMixins],
   components: {
     Navbar,
     Footer,
+    GoDesktopPage,
+  },
+  computed: {
+    isShowGoToDesktopPage() {
+      return this.isNotDesktop && this.isOnAdminUrl;
+    },
   },
 };
 </script>
@@ -55,6 +69,10 @@ body {
     position: relative;
     width: 300px;
 
+    @include mobile {
+      width: 225px;
+    }
+
     ul {
       display: flex;
       justify-content: center;
@@ -71,6 +89,12 @@ body {
     li {
       transform: rotate(45deg);
       display: inline-block;
+
+      .v-icon {
+        @include mobile {
+          font-size: 1rem !important;
+        }
+      }
     }
   }
 }
