feb9a2bb83299edef19e84fcb03f8b1dd4d69092
Chen Frederick <frederickfd88@gmail.com>
2022-05-18 21:26:29 +0700

    Vuex Setup with Vuex Modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,40 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Cookies from "universal-cookie";
+
 import FacilitiesStore from "./facilities-store";
+import ProgramsStore from "./programs-store";
+import BookCollectionsStore from "./book-collections-store";
+import PartnersStore from "./partners-store";
+import LibraryDetailsStore from "./library-details-store";
 
 Vue.use(Vuex);
+const cookies = new Cookies();
 
 export default new Vuex.Store({
   modules: {
     facilities: FacilitiesStore,
+    programs: ProgramsStore,
+    bookCollections: BookCollectionsStore,
+    partners: PartnersStore,
+    libraryDetails: LibraryDetailsStore,
   },
-  state: {},
+  state: {
+    adminToken: cookies.get("admin-token"),
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    setAdminToken(state, token) {
+      cookies.set("admin-token", token, {
+        path: "/", // use / as the path if you want your cookie to be accessible on all pages
+        expires: new Date(Date.now() + 2592000), // 1 day
+      });
+      state.adminToken = token;
+    },
+    doLogout(state) {
+      cookies.remove("admin-token", { path: "/" });
+      state.adminToken = "";
+    },
+  },
   actions: {},
 });
