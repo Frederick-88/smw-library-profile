66b29deeed0b64929b306ebe7cb7ded306875341
Chen Frederick <frederickfd88@gmail.com>
2022-05-18 21:26:56 +0700

    Library Facilities Vuex Setup

--- src/store/facilities-store.js
+++ src/store/facilities-store.js
@@ -1,46 +1,87 @@
 import {
   database,
-  storageRef,
-  createFacility,
-  updateFacility,
-  deleteFacility,
+  fbCreateFacility,
+  fbUpdateFacility,
+  fbDeleteFacility,
 } from "@/firebase/db";
 
 const FacilitiesStore = {
   namespaced: true,
   state: {
     facilityList: [],
-    isFetchingFacilities: false,
+    isFetchingFacility: false,
+    isDeletingFacility: false,
   },
   getters: {},
   mutations: {
-    setFacilities(state, data) {
+    setFacilityList(state, data) {
       state.facilityList = data;
     },
-    setIsFetchingFacilities(state, boolean) {
-      state.isFetchingFacilities = boolean;
+    setIsFetchingFacility(state, boolean) {
+      state.isFetchingFacility = boolean;
+    },
+    setIsDeletingFacility(state, boolean) {
+      state.isDeletingFacility = boolean;
     },
   },
   actions: {
-    async fetchFacilities({ commit }) {
-      commit("setIsFetchingFacilities", true);
+    async fetchFacility({ commit }) {
+      commit("setIsFetchingFacility", true);
 
       database
         .collection("library-facilities")
         .get()
         .then((querySnapshot) => {
-          const results = querySnapshot.docs;
-          commit("setFacilities", results);
+          const results = querySnapshot.docs.map((doc) => {
+            return { id: doc.id, ...doc.data() };
+          });
+          results.sort((a, b) => a.title.localeCompare(b.title)); // sort by alphabetical order
+
+          commit("setFacilityList", results);
         })
         .catch((error) => {
           console.error(error);
         })
         .finally(() => {
-          commit("setIsFetchingFacilities", false);
+          commit("setIsFetchingFacility", false);
         });
     },
-    async createFacility({ commit }, data) {
-      console.log(data);
+    async createFacility({ dispatch, commit }, data) {
+      try {
+        commit("setIsFetchingFacility", true);
+
+        await fbCreateFacility(data);
+        dispatch("fetchFacility");
+
+        this._vm.$toast.success("Successfully Created Facility.");
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async updateFacility({ dispatch, commit }, data) {
+      try {
+        commit("setIsFetchingFacility", true);
+
+        await fbUpdateFacility(data.id, data);
+        dispatch("fetchFacility");
+
+        this._vm.$toast.success("Successfully Updated Facility.");
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteFacility({ dispatch, commit }, id) {
+      try {
+        commit("setIsDeletingFacility", true);
+        await fbDeleteFacility(id);
+        commit("setIsDeletingFacility", false);
+
+        dispatch("fetchFacility");
+
+        this._vm.$toast.success("Successfully Deleted Facility.");
+      } catch (error) {
+        console.error(error);
+      }
     },
   },
 };
