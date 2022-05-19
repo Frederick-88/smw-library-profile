8c6f96c7e17eb894f435beef971f773db995e867
Chen Frederick <frederickfd88@gmail.com>
2022-05-19 23:30:49 +0700

    Done Footer

--- src/components/base/Footer.vue
+++ src/components/base/Footer.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-footer class="footer py-3 px-10 black d-flex justify-space-between">
+  <v-footer
+    class="footer black d-flex py-3 px-2 px-sm-10 justify-center justify-sm-space-between"
+  >
     <h5 class="footer-text white--text">
       © 2022 All Rights Reserved, SMA Maitreyawira Library
     </h5>
     <img
-      class="footer-logo"
+      class="footer-logo d-none d-sm-block"
       :src="require('@/assets/images/smw-small-logo.png')"
     />
   </v-footer>
@@ -18,10 +20,17 @@ export default {
 
 <style lang="scss">
 .footer {
+  height: 80px;
+
   .footer-text {
     font-size: 0.875rem;
     font-weight: 500;
     margin-bottom: -4px;
+
+    @include mobile {
+      font-size: 0.8125rem;
+      text-align: center;
+    }
   }
 
   .footer-logo {
