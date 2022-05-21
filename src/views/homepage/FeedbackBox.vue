3f3edb07d35a24e3f0bd066048353aaa87914060
Chen Frederick <frederickfd88@gmail.com>
2022-05-21 12:28:53 +0700

    Done FeedbackBox Views Homepage

---
+++ src/views/homepage/FeedbackBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-main class="pa-0">
+    <feedback-box-page />
+  </v-main>
+</template>
+
+<script>
+import FeedbackBoxPage from "@/components/FeedbackBoxPage.vue";
+
+export default {
+  name: "FeedbackBox",
+  components: { FeedbackBoxPage },
+};
+</script>
