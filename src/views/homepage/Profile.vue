e3a20720ffd0775eb50fb12f40ec468b054d21d9
Chen Frederick <frederickfd88@gmail.com>
2022-05-21 12:29:11 +0700

    Done Profile Views Homepage

---
+++ src/views/homepage/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-main class="pa-0">
+    <profile-banner />
+    <profile-vision-and-mission />
+    <profile-history />
+    <profile-organization-structure />
+  </v-main>
+</template>
+
+<script>
+import ProfileBanner from "@/components/profile/ProfileBanner.vue";
+import ProfileVisionAndMission from "@/components/profile/ProfileVisionAndMission.vue";
+import ProfileHistory from "@/components/profile/ProfileHistory.vue";
+import ProfileOrganizationStructure from "@/components/profile/ProfileOrganizationStructure.vue";
+
+export default {
+  name: "Profile",
+  components: {
+    ProfileBanner,
+    ProfileVisionAndMission,
+    ProfileHistory,
+    ProfileOrganizationStructure,
+  },
+};
+</script>
