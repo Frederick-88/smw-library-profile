451e55280c1480cc4f1955503484a600b5e813ce
Chen Frederick <frederickfd88@gmail.com>
2022-05-21 12:28:38 +0700

    Done ContactUs Views Homepage

---
+++ src/views/homepage/ContactUs.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-main class="pa-0">
+    <contact-us-page />
+  </v-main>
+</template>
+
+<script>
+import ContactUsPage from "@/components/ContactUsPage.vue";
+
+export default {
+  name: "ContactUs",
+  components: { ContactUsPage },
+};
+</script>
