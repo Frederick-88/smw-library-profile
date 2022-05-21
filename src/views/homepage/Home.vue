667409c686a1fcc4b15591889d9fa649b02160e0
Chen Frederick <frederickfd88@gmail.com>
2022-05-21 12:29:00 +0700

    Done Home Views Homepage

---
+++ src/views/homepage/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-main class="pa-0">
+    <home-banner />
+    <home-facilities />
+    <home-open-hours />
+    <home-programs />
+    <home-partners />
+    <home-book-collections />
+    <home-maps />
+  </v-main>
+</template>
+
+<script>
+import HomeBanner from "@/components/home/HomeBanner.vue";
+import HomeFacilities from "@/components/home/HomeFacilities.vue";
+import HomeOpenHours from "@/components/home/HomeOpenHours.vue";
+import HomePrograms from "@/components/home/HomePrograms.vue";
+import HomePartners from "@/components/home/HomePartners.vue";
+import HomeBookCollections from "@/components/home/HomeBookCollections.vue";
+import HomeMaps from "@/components/home/HomeMaps.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HomeBanner,
+    HomeFacilities,
+    HomeOpenHours,
+    HomePrograms,
+    HomePartners,
+    HomeBookCollections,
+    HomeMaps,
+  },
+};
+</script>
