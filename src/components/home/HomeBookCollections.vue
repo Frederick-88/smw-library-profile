65e3956d8a6d06d092d65bf24aa5853b2f4fdd0c
Chen Frederick <frederickfd88@gmail.com>
2022-06-10 00:18:44 +0700

    Updated Elibrary Link in  HomeBookCollections

--- src/components/home/HomeBookCollections.vue
+++ src/components/home/HomeBookCollections.vue
@@ -25,7 +25,7 @@
             class="book-collections-item__container"
             v-for="bookCollection in bookCollectionList"
             :key="bookCollection.id"
-            href="https://elibrary.uvers.ac.id"
+            href="http://elibrary.uvers.ac.id/elibrary/"
             target="_blank"
           >
             <v-card class="book-collections-item">
